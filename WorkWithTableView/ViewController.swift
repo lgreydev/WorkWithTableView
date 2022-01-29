28aa89c865c42adeebd445188e5fc6173993fd3b
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-01-29 23:35:23 +0200

    Initial Commit

---
+++ WorkWithTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkWithTableView
+//
+//  Created by Sergey Lukaschuk on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
