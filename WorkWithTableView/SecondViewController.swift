ef52d0d5cfe3c90c3d01e1607c48f70b2565c90f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-02-01 18:25:54 +0200

    add second TableView

--- WorkWithTableView/SecondViewController.swift
+++ WorkWithTableView/SecondViewController.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let identifier = "myCell"
+    
+    let weather = [
+        "Sun ☀️",
+        "Clouds ⛅️",
+        "Rain 🌧",
+        "Snow ❄️",
+        "Thunderstorm ⚡️"
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+// MARK: - Delegate DataSource
+extension SecondViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        weather.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = weather[indexPath.row]
+        return cell
+    }
+    
+    
 }
